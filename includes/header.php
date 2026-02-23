892a5725ec4457e8b84fa7289ee7d33848d7815a
Alexander Nekrasov <nekrasoft.kirov@gmail.com>
2026-02-23 22:08:34 +0300

    feat(site): mvp-ext update2

--- includes/header.php
+++ includes/header.php
@@ -226,6 +226,17 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-KCJQ2RK9');</script>
     <!-- End Google Tag Manager -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L6NJ6NWM3R"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-L6NJ6NWM3R');
+    </script>
+
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
